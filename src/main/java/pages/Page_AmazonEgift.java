2adcb1433f503ee1072f3eb71d8421b35f81ff43
Ashwini Poornima <niresh0427@gmail.com>
2024-07-15 20:23:44 +0530

    feat: updated code

--- src/main/java/pages/Page_AmazonEgift.java
+++ src/main/java/pages/Page_AmazonEgift.java
@@ -5,136 +5,153 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-
-import base.Core;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Page_AmazonEgift {
 
-	static WebDriver driver;
+	private static WebDriver driver;
 
-	public static void search_google(String searchlink) {
+	public static void startUp() {
 
-		System.setProperty("webdriver.chrome.driver",
-				"D:\\Program Files (x86)\\Eclipse\\eclipse-workspace\\AmazonEgiftAutomation\\drivers\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup(); // Manage the WebDriver automatically
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com");
 
-		driver.findElement(By.id("APjFqb")).sendKeys(searchlink);
 	}
 
-	public static void click_search() {
+	public static void searchGoogle(String searchlink) {
 
-		driver.findElement(By.id("APjFqb")).submit();
+		WebElement txtGsearch = driver.findElement(By.id("APjFqb"));
+		txtGsearch.sendKeys(searchlink); // Search Amazon on Google search
 	}
 
-	public static void get_link(String weblink) throws InterruptedException {
+	public static void clickSearch() {
+
+		WebElement btnSearch = driver.findElement(By.id("APjFqb"));
+		btnSearch.submit();
+	}
+
+	public static void getLink(String weblink) throws InterruptedException {
+
 		WebElement searchBox = driver.findElement(By.xpath("//span[contains(text(),'" + weblink + "')]"));
 		searchBox.click();
-		Thread.sleep(2000);
-	
+		Thread.sleep(5000);
+
 	}
 
-	public static void click_deliver() throws InterruptedException {
+	public static void clickDeliver() throws InterruptedException {
 
-		WebElement button_deliverto = driver.findElement(By.id("nav-global-location-popover-link"));
+		WebElement btnDeliverto = driver.findElement(By.id("nav-global-location-popover-link"));
 
-		if (button_deliverto.isDisplayed()) {
-			button_deliverto.click();
+		if (btnDeliverto.isDisplayed()) {
+			btnDeliverto.click();
 		} else {
+			Thread.sleep(5000);
 			// Click on "Your Account" button
-			WebElement button_yourAccount = driver.findElement(By.xpath("//a[contains(text(),'Your Account')]"));
-			button_yourAccount.click();
-
+			WebElement btnYouraccount = driver.findElement(By.xpath("//a[contains(text(),'Your Account')]"));
+			btnYouraccount.click();
+			Thread.sleep(5000);
 			// Click on "Deliver to" button again after navigating
-			button_deliverto.click();
+			btnDeliverto.click();
 		}
-		
+
 		Thread.sleep(5000);
 
 	}
 
-	public static void select_country(String country) {
-		
-		driver.findElement(By.xpath("//span[@class=\"a-button-text a-declarative\"]")).click();
-		WebElement select_country = driver.findElement(By.xpath("//a[contains(text(),'"+ country +"')]"));
-		select_country.click();	
-		
+	public static void selectCountry(String country) {
+
+		WebElement btnDropdown = driver.findElement(By.xpath("//span[@class=\"a-button-text a-declarative\"]"));
+		btnDropdown.click();
+		WebElement selectCountry = driver.findElement(By.xpath("//a[contains(text(),'" + country + "')]"));
+		selectCountry.click();
+
 	}
-	
-	public static void click_done () {
-		
-		driver.findElement(By.xpath("//button[@name='glowDoneButton']")).click();
-		
+
+	public static void clickDone() {
+
+		WebElement btnDone = driver.findElement(By.xpath("//button[@name='glowDoneButton']"));
+		btnDone.click();
+
 	}
-	
-	public static void verify_country (String dCountry) {
-		
+
+	public static void verifyCountry(String dCountry) {
+
 		// Wait for the deliver to button to update with the selected country
-        WebElement change_country = driver.findElement(By.xpath("//span[contains(text(),'"+ dCountry +"')]"));
-        //change_country.isDisplayed();
-        String button_text = change_country.getText();
-        button_text.contains(dCountry);
-        
+		WebElement changeCountry = driver.findElement(By.xpath("//span[contains(text(),'" + dCountry + "')]"));
+		// change_country.isDisplayed();
+		String btnText = changeCountry.getText();
+		btnText.contains(dCountry);
+
 	}
-	
-	public static void search_amazon(String searchtext) {
-		
-		driver.findElement(By.id("twotabsearchtextbox")).sendKeys(searchtext);
-		
-	}
-	
-	public static void click_Asearch() {
-		
-		driver.findElement(By.id("nav-search-submit-button")).submit();
+
+	public static void searchAmazon(String searchtext) {
+
+		WebElement txtAsearch = driver.findElement(By.id("twotabsearchtextbox"));
+		txtAsearch.sendKeys(searchtext);
 		
 	}
-	
-	public static void click_result(String giftcard) throws InterruptedException {
-		
-		WebElement click_item = driver.findElement(By.xpath("//span[contains(text(),'" + giftcard + "')]"));
-		click_item.click();
+
+	public static void clickAsearch() {
+
+		WebElement btnAsearch = driver.findElement(By.id("nav-search-submit-button"));
+		btnAsearch.submit();
+
+	}
+
+	public static void clickResult(String giftcard) throws InterruptedException {
+
+		WebElement btnGiftcard = driver.findElement(By.xpath("//span[contains(text(),'" + giftcard + "')]"));
+		btnGiftcard.click();
 		Thread.sleep(5000);
-		
+
 	}
-	
-	public static void click_giftcard () throws InterruptedException {
-		
-		driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]/div[2]/div/div/span/div/div/div[2]/span/a/div/img")).click();
+
+	public static void clickGiftcard() throws InterruptedException {
+
+		WebElement btnGiftcardclick = driver
+				.findElement(By.xpath("//img[contains(@alt, 'Amazon.com eGift Card') and (@data-image-index='1')]"));
+		btnGiftcardclick.click();
 		Thread.sleep(5000);
-		
+
 	}
-	
-	public static void verify_giftcard (String design) throws InterruptedException {
-		
-		WebElement card_design = driver.findElement(By.xpath("//img[@alt='"+ design +"']"));
-		card_design.click();
-		String design_text = card_design.getText();
-		
-		WebElement verify_design = driver.findElement(By.xpath("//span[@id='gc-design-title']"));
-		String dispalyed_designtext = verify_design.getText();
-		
-		Assert.assertEquals(dispalyed_designtext, design_text);
+
+	public static void verifyGiftcard(String design) throws InterruptedException {
+
+		WebElement cardDesign = driver.findElement(By.xpath("//img[@alt='" + design + "']"));
+		cardDesign.click();
+		String txtDesign = cardDesign.getAttribute("alt");
+
+		WebElement verifyDesign = driver.findElement(By.xpath("//span[@id='gc-design-title']"));
+		String txtDisplayeddesign = verifyDesign.getText();
+
+		Assert.assertEquals(txtDisplayeddesign, txtDesign);
 		Thread.sleep(5000);
-        //button_text.contains(design);
-		
+
+	}
+
+	public static void selectAmount(String amount) {
+
+		WebElement btnAmount = driver.findElement(By.xpath("//button[contains(text(),'" + amount + "')]"));
+		((ChromeDriver) driver).executeScript("arguments[0].scrollIntoView();", btnAmount); // Scroll to find element
+		btnAmount.click();
+
+	}
+
+	public static void verifyAmount() {
+
+		WebElement txtAmount = driver.findElement(By.xpath("//span[contains(text(),'$75.00')]"));
+		((ChromeDriver) driver).executeScript("arguments[0].scrollIntoView();", txtAmount); // Scroll to find element
+		txtAmount.isDisplayed();
+
 	}
 	
-	public static void select_amount(String amount) {
+	public static void closeUp() throws InterruptedException {
 		
-		WebElement click_amount = driver.findElement(By.xpath("//button[contains(text(),'"+ amount +"')]"));
-		((ChromeDriver) driver).executeScript("arguments[0].scrollIntoView();", click_amount);
-		click_amount.click();
-		
+		Thread.sleep(5000);
+		driver.quit();
+
 	}
-	
-	public static void verify_amount() {
-		
-		WebElement change_country = driver.findElement(By.xpath("//span[contains(text(),'$75.00')]"));
-        change_country.isDisplayed();
-		
-	}
-	
-	
 
 }
--- src/test/java/stepDefinitions/StepDefenition_AmazonEgidt.java
+++ src/test/java/stepDefinitions/StepDefenition_AmazonEgidt.java
@@ -6,77 +6,93 @@ import io.cucumber.java.en.When;
 import pages.Page_AmazonEgift;
 
 public class StepDefenition_AmazonEgidt {
-	
+
 	@Given("User enter {string} on Google search")
 	public void user_enter_on_google_search(String searchlink) {
-		Page_AmazonEgift.search_google(searchlink);
+		Page_AmazonEgift.startUp();
+		Page_AmazonEgift.searchGoogle(searchlink);
+
 	}
 
 	@When("User click on search button")
 	public void user_click_on_search_button() {
-	    Page_AmazonEgift.click_search();
+		Page_AmazonEgift.clickSearch();
+
 	}
 
 	@When("User select the {string} link in the search results and click")
 	public void user_select_the_link_in_the_search_results_and_click(String weblink) throws InterruptedException {
-	    Page_AmazonEgift.get_link(weblink);
+		Page_AmazonEgift.getLink(weblink);
+
 	}
 
 	@When("User click on the Deliver To button")
 	public void user_click_on_the_Deliver_To_button() throws InterruptedException {
-	   Page_AmazonEgift.click_deliver();
+		Page_AmazonEgift.clickDeliver();
+
 	}
 
 	@When("User select {string} as a country")
 	public void user_select_as_a_country(String country) {
-		Page_AmazonEgift.select_country(country);
-	    
+		Page_AmazonEgift.selectCountry(country);
+
 	}
 
 	@When("User click done button")
 	public void user_click_done_button() {
-	    Page_AmazonEgift.click_done();
+		Page_AmazonEgift.clickDone();
+
 	}
 
 	@Then("User verify Deliver to updated with {string} country name")
 	public void user_verify_Deliver_to_updated_with_country_name(String dCountry) {
-	    Page_AmazonEgift.verify_country(dCountry);
+		Page_AmazonEgift.verifyCountry(dCountry);
+
 	}
 
 	@Given("User enter {string} on Amazon search")
 	public void user_enter_on_amazon_search(String searchtext) {
-	    Page_AmazonEgift.search_amazon(searchtext);
+		Page_AmazonEgift.searchAmazon(searchtext);
+
 	}
 
 	@When("User click search button")
 	public void user_click_search_button() {
-	    Page_AmazonEgift.click_Asearch();
+		Page_AmazonEgift.clickAsearch();
+
 	}
 
 	@When("User click on {string} button")
 	public void user_click_on_button(String giftcard) throws InterruptedException {
-		Page_AmazonEgift.click_result(giftcard);
-	    
+		Page_AmazonEgift.clickResult(giftcard);
+
 	}
 
 	@When("User select a gift card")
 	public void user_select_a_gift_card() throws InterruptedException {
-	    Page_AmazonEgift.click_giftcard();
+		Page_AmazonEgift.clickGiftcard();
+
 	}
 
 	@Then("User verify the preview image should be updated with the selected design {string}")
-	public void user_verify_the_preview_image_should_be_updated_with_the_selected_design(String design) throws InterruptedException {
-	    Page_AmazonEgift.verify_giftcard(design);
+	public void user_verify_the_preview_image_should_be_updated_with_the_selected_design(String design)
+			throws InterruptedException {
+		Page_AmazonEgift.verifyGiftcard(design);
+
 	}
 
 	@When("User select a different amount like {string}")
 	public void user_select_a_different_amount_like(String amount) {
-	    Page_AmazonEgift.select_amount(amount);
+		Page_AmazonEgift.selectAmount(amount);
+
 	}
 
 	@Then("User verify preview image value should be updated with the new amount {string}")
-	public void user_verify_preview_image_value_should_be_updated_with_the_new_amount(String string) {
-	    Page_AmazonEgift.verify_amount();
+	public void user_verify_preview_image_value_should_be_updated_with_the_new_amount(String string)
+			throws InterruptedException {
+		Page_AmazonEgift.verifyAmount();
+		Page_AmazonEgift.closeUp();
+
 	}
 
 }
